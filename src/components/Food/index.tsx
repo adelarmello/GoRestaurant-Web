3d65e06ab16dbc7d5b537f0b8c308e79c3188c38
Adelar <adelarmello8@gmail.com>
2020-08-24 14:51:35 -0300

    Alterando a disponibilidade do prato

--- src/components/Food/index.tsx
+++ src/components/Food/index.tsx
@@ -4,6 +4,8 @@ import { FiEdit3, FiTrash } from 'react-icons/fi';
 
 import { Container } from './styles';
 
+import api from '../../services/api';
+
 interface IFoodPlate {
   id: number;
   name: string;
@@ -28,6 +30,16 @@ const Food: React.FC<IProps> = ({
 
   async function toggleAvailable(): Promise<void> {
     // TODO UPDATE STATUS (available)
+    try {
+      await api.put(`/foods/${food.id}`, {
+        ...food,
+        available: !isAvailable,
+      });
+
+      setIsAvailable(!isAvailable);
+    } catch (err) {
+      console.log();
+    }
   }
 
   function setEditingFood(): void {
